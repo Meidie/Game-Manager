2bf670040c233e579ac6b7dab7e3b76741120e78
Matúš <matuspohancenik@gmail.com>
2018-07-23 22:19:21 +0200

    Vytvaranie plochy

--- src/InputFromKeyboard.java
+++ src/InputFromKeyboard.java
@@ -3,14 +3,14 @@ import java.io.InputStreamReader;
 
 public class InputFromKeyboard {
 
-    public static int readInt(String napis_pre_uzivatela)
+    public static int readInt(String user_input)
     {
         int n = 0;
         String s;
 
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         try
-        {	System.out.println(napis_pre_uzivatela);
+        {	System.out.println(user_input);
             s = input.readLine();
             System.out.println("Nacital som " + s);
             n = Integer.parseInt(s);
@@ -18,7 +18,7 @@ public class InputFromKeyboard {
         catch (Exception e)
         {
             System.out.println("nepodarilo sa");
-            n = readInt(napis_pre_uzivatela);
+            n = readInt(user_input);
         }
 
         return n;
